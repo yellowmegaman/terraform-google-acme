17e0f7aab180b5d502b8764b3e5250932dacd77b
yellowmegaman <yellowmegaman@workbox>
2019-07-05 01:18:15 +0300

    0.12 stuff and only certificate pinning

--- main.tf
+++ main.tf
@@ -1,30 +1,26 @@
 provider "acme" {
-  server_url      = "${var.acme_url}"
-}
-
-resource "tls_private_key" "reg_private_key" {
-  algorithm       = "RSA"
+  server_url      = var.acme_url
 }
 
 resource "acme_registration" "reg" {
-  account_key_pem = "${var.account_key != "" ? var.account_key : tls_private_key.reg_private_key.private_key_pem }"
-  email_address   = "${var.email}"
+  account_key_pem = var.account_key
+  email_address   = var.email
 }
 
 resource "tls_cert_request" "req" {
   key_algorithm   = "RSA"
-  private_key_pem = "${file("${var.ssl_private_key != "" ? var.ssl_private_key : ""}")}"
+  private_key_pem = file(var.ssl_private_key)
   subject {
-    common_name   = "${var.common_name}"
-    organization  = "${var.organization}"
+    common_name   = var.common_name
+    organization  = var.organization
   }
 }
 
 resource "acme_certificate" "certificate" {
-  account_key_pem           = "${acme_registration.reg.account_key_pem}"
-  certificate_request_pem   = "${tls_cert_request.req.cert_request_pem}"
-  min_days_remaining        = "${var.min_days}"
+  account_key_pem           = acme_registration.reg.account_key_pem
+  certificate_request_pem   = tls_cert_request.req.cert_request_pem
+  min_days_remaining        = var.min_days
   dns_challenge {
-    provider                = "${var.cloud}"
+    provider                = var.cloud
   }
 }
