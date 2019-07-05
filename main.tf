68b9526570a6c216faec0df8bbbcdb9ef0789e0f
yellowmegaman <yellowmegaman@workbox>
2019-07-05 03:00:29 +0300

    0.12 fixes

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "acme" {
-  server_url      = var.acme_url
+  server_url      = "var.acme_url"
 }
 
 resource "tls_private_key" "reg_private_key" {
@@ -7,24 +7,24 @@ resource "tls_private_key" "reg_private_key" {
 }
 
 resource "acme_registration" "reg" {
-  account_key_pem = tls_private_key.reg_private_key.private_key_pem
-  email_address   = var.email
+  account_key_pem = "tls_private_key.reg_private_key.private_key_pem"
+  email_address   = "var.email"
 }
 
 resource "tls_cert_request" "req" {
   key_algorithm   = "RSA"
-  private_key_pem = file(var.ssl_private_key)
+  private_key_pem = "file(var.ssl_private_key)"
   subject {
-    common_name   = var.common_name
-    organization  = var.organization
+    common_name   = "var.common_name"
+    organization  = "var.organization"
   }
 }
 
 resource "acme_certificate" "certificate" {
-  account_key_pem           = acme_registration.reg.account_key_pem
-  certificate_request_pem   = tls_cert_request.req.cert_request_pem
-  min_days_remaining        = var.min_days
+  account_key_pem           = "acme_registration.reg.account_key_pem"
+  certificate_request_pem   = "tls_cert_request.req.cert_request_pem"
+  min_days_remaining        = "var.min_days"
   dns_challenge {
-    provider                = var.cloud
+    provider                = "var.cloud"
   }
 }
